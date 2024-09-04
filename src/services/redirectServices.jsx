56abf71d9fb01d3c7f65943768c9fa5127db80e1
ciprian manea <ciprian.manea@netrom.live>
2024-09-04 17:03:19 +0300

    FIX: finshed with icons

--- src/services/redirectServices.jsx
+++ src/services/redirectServices.jsx
@@ -1,20 +1,20 @@
 export const openFacebookProfile = () => {
     const facebookProfileUrl = 'https://www.facebook.com/share/Akd4JNZg4mohpx3E/?mibextid=LQQJ4d';
-    window.open(facebookProfileUrl);
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        window.location.href = facebookProfileUrl;
+    } else {
+        window.open(instagramProfileUrl, '_blank');
+    }
 };
 
 export const openInstagramProfile = () => {
     const instagramProfileUrl = 'https://www.instagram.com/micronica.2004';
-    const instagramAppUrl = 'instagram://user?username=micronica.2004';
-
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     if (isMobile) {
-        window.location.href = instagramAppUrl;
-
-        setTimeout(() => {
-            window.location.href = instagramProfileUrl;
-        }, 500);
+        window.location.href = instagramProfileUrl;
     } else {
         window.open(instagramProfileUrl, '_blank');
     }
@@ -29,7 +29,14 @@ export const openEmailClient = () => {
 export const openMapsLocation = () => {
     const placeName = encodeURIComponent('MICRONICA S.R.L., Hala 1, Str. Gării nr. 10, Sânnicolau Mare 305600');
     const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${placeName}`;
-    window.open(googleMapsUrl, '_blank');
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        window.location.href = googleMapsUrl;
+    } else {
+        window.open(googleMapsUrl, '_blank');
+    }
 };
 
 export const openPhoneDialer = () => {
